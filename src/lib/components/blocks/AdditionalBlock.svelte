bc51785fd9c3b6dc78cd5a47de164e88af769948
Parrot Live user <user@localhost.localdomain>
2025-10-27 10:28:12 +0000

    Micro.Hello

---
+++ src/lib/components/blocks/AdditionalBlock.svelte
@@ -0,0 +1,21 @@
+<script>
+    var {name, value} = $props(); 
+</script>
+
+<div>
+    <span class="name">{name}</span>
+    <span class="value">{value}</span>
+</div>
+
+<style>
+    div{
+        background-color: var(--gray-color);
+        border-radius: var(--std-radius);
+        padding: 16px;
+        display: flex;
+        justify-content: space-between;
+    }
+    .value{
+        font-weight: bold;
+    }
+</style>
--- src/routes/clubs/micro/+page.svelte
+++ src/routes/clubs/micro/+page.svelte
@@ -1,5 +1,6 @@
 <script>
   import TitleBlock from '$lib/components/blocks/TitleBlock.svelte';
+  import AdditionalBlock from '$lib/components/blocks/AdditionalBlock.svelte';
 </script>
 
 <TitleBlock
@@ -8,7 +9,44 @@
   text="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ad incidunt maiores non pariatur iure aperiam voluptatum officiis ducimus."
 />
 
+<section class="additional">
+  <AdditionalBlock
+    name="Data"
+    value="12.23.2095"
+  />
+  <AdditionalBlock
+    name="Data"
+    value="12.23.2095"
+  />
+  <AdditionalBlock
+    name="Data"
+    value="12.23.2095"
+  />
+</section>
+<section class="welcome_img">
+  <img src="/Img/bg/(3).webp" alt="welcom img">
+</section>
+<section class="article">
+  
+</section>
+
 
 <style>
+  .additional{
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    gap: var(--std-space);
+  }
 
+  .welcome_img{
+    height: 600px;
+  }
+
+  .welcome_img > img{
+    width: 100%;
+    height: 100%;
+    object-fit: cover; 
+    display: block;
+    border-radius: var(--std-radius);
+  }
 </style>
