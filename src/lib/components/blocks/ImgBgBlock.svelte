fc162773bbc4ce5198b620fe884140ab323c7dfd
WladekW <bagdatwl@gmail.com>
2025-12-15 15:54:39 +0100

    optimization ;}

--- src/lib/components/blocks/ImgBgBlock.svelte
+++ src/lib/components/blocks/ImgBgBlock.svelte
@@ -1,23 +1,20 @@
 <script>
-  import favicon from "$lib/assets/icons/arrow24dp.svg";
-  import ArrowImg from "$lib/components/ui/ArrowLink.svelte";
-
-  var { src, children, className } = $props();
+    var { src, children, className } = $props();
 </script>
 
 <div style="background-image: url('{src}');" class={className}>
-  {@render children?.()}
+    {@render children?.()}
 </div>
 
 <style>
-  div {
-    background-image: url("/Img/koyasan.webp");
-    background-repeat: no-repeat;
-    background-size: cover;
-    background-position: center;
-    border-radius: var(--std-radius);
-    display: flex;
-    align-items: end;
-    justify-content: end;
-  }
+    div {
+        background-image: url("/Img/koyasan.webp");
+        background-repeat: no-repeat;
+        background-size: cover;
+        background-position: center;
+        border-radius: var(--std-radius);
+        display: flex;
+        align-items: end;
+        justify-content: end;
+    }
 </style>
