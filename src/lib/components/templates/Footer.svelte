79f6c8808cf55a722c6b6afaaf994e2fd5a9a165
WladekW <bagdatwl@gmail.com>
2025-12-15 15:59:11 +0100

    add /projects to menu

--- src/lib/components/templates/Footer.svelte
+++ src/lib/components/templates/Footer.svelte
@@ -1,196 +1,196 @@
 <script>
-  import SocialMediaBlock from "$lib/components/blocks/SocialMediaBlock.svelte";
+    import SocialMediaBlock from "$lib/components/blocks/SocialMediaBlock.svelte";
 </script>
 
 <footer>
-  <iframe
-    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2011.62271940284!2d22.061026890411775!3d53.15980381419067!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x471e347628cf15fd%3A0x3d81b353e3859a67!2zWmVzcMOzxYIgU3prw7PFgiBNZWNoYW5pY3pueWNoIGkgT2fDs2xub2tzenRhxYJjxIVjeWNoIG5yIDUgdyDFgW9txbx5!5e0!3m2!1spl!2sus!4v1760870636002!5m2!1spl!2sus"
-    allowfullscreen=""
-    loading="lazy"
-    referrerpolicy="no-referrer-when-downgrade"
-    title="school location"
-  >
-  </iframe>
-  <div class="footer--content">
-    <nav class="footer_menu">
-      <ul class="footer_menu--list">
-        <li>
-          <a href="/"><h5>Home</h5></a>
-        </li>
-        <li>
-          <a href="/"><span>Section</span></a>
-        </li>
-        <li>
-          <a href="/"><span>Section</span></a>
-        </li>
-        <li>
-          <a href="/"><span>Section</span></a>
-        </li>
-      </ul>
-      <ul class="footer_menu--list">
-        <li>
-          <a href="/clubs"><h5>Clubs</h5></a>
-        </li>
-        <li>
-          <a href="/"><span>Section</span></a>
-        </li>
-        <li>
-          <a href="/"><span>Section</span></a>
-        </li>
-        <li>
-          <a href="/"><span>Section</span></a>
-        </li>
-        <li>
-          <a href="/"><span>Section</span></a>
-        </li>
-      </ul>
-      <ul class="footer_menu--list">
-        <li>
-          <a href="/"><h5>Projects</h5></a>
-        </li>
-        <li>
-          <a href="/"><span>Section</span></a>
-        </li>
-        <li>
-          <a href="/"><span>Section</span></a>
-        </li>
-        <li>
-          <a href="/"><span>Section</span></a>
-        </li>
-        <li>
-          <a href="/"><span>Section</span></a>
-        </li>
-        <li>
-          <a href="/"><span>Section</span></a>
-        </li>
-      </ul>
-      <ul class="footer_menu--list">
-        <li>
-          <a href="/calendar"><h5>Calendar</h5></a>
-        </li>
-        <li>
-          <a href="/"><span>Section</span></a>
-        </li>
-        <li>
-          <a href="/"><span>Section</span></a>
-        </li>
-        <li>
-          <a href="/"><span>Section</span></a>
-        </li>
-        <li>
-          <a href="/"><span>Section</span></a>
-        </li>
-      </ul>
-      <ul class="footer_menu--list">
-        <li>
-          <a href="/"><h5>Contact Us</h5></a>
-        </li>
-        <li>
-          <a href="/"><span>Section</span></a>
-        </li>
-      </ul>
-    </nav>
-    <div class="footer_policy">
-      <a href="/">© 2025 ZSMiO nr 5</a>
-      <a href="/terms">Terms</a>
-      <a href="/security">Security</a>
-      <a href="/privacy">Privacy</a>
+    <iframe
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2011.62271940284!2d22.061026890411775!3d53.15980381419067!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x471e347628cf15fd%3A0x3d81b353e3859a67!2zWmVzcMOzxYIgU3prw7PFgiBNZWNoYW5pY3pueWNoIGkgT2fDs2xub2tzenRhxYJjxIVjeWNoIG5yIDUgdyDFgW9txbx5!5e0!3m2!1spl!2sus!4v1760870636002!5m2!1spl!2sus"
+        allowfullscreen=""
+        loading="lazy"
+        referrerpolicy="no-referrer-when-downgrade"
+        title="school location"
+    >
+    </iframe>
+    <div class="footer--content">
+        <nav class="footer_menu">
+            <ul class="footer_menu--list">
+                <li>
+                    <a href="/"><h5>Home</h5></a>
+                </li>
+                <li>
+                    <a href="/"><span>Section</span></a>
+                </li>
+                <li>
+                    <a href="/"><span>Section</span></a>
+                </li>
+                <li>
+                    <a href="/"><span>Section</span></a>
+                </li>
+            </ul>
+            <ul class="footer_menu--list">
+                <li>
+                    <a href="/clubs"><h5>Clubs</h5></a>
+                </li>
+                <li>
+                    <a href="/"><span>Section</span></a>
+                </li>
+                <li>
+                    <a href="/"><span>Section</span></a>
+                </li>
+                <li>
+                    <a href="/"><span>Section</span></a>
+                </li>
+                <li>
+                    <a href="/"><span>Section</span></a>
+                </li>
+            </ul>
+            <ul class="footer_menu--list">
+                <li>
+                    <a href="/projects"><h5>Projects</h5></a>
+                </li>
+                <li>
+                    <a href="/"><span>Section</span></a>
+                </li>
+                <li>
+                    <a href="/"><span>Section</span></a>
+                </li>
+                <li>
+                    <a href="/"><span>Section</span></a>
+                </li>
+                <li>
+                    <a href="/"><span>Section</span></a>
+                </li>
+                <li>
+                    <a href="/"><span>Section</span></a>
+                </li>
+            </ul>
+            <ul class="footer_menu--list">
+                <li>
+                    <a href="/calendar"><h5>Calendar</h5></a>
+                </li>
+                <li>
+                    <a href="/"><span>Section</span></a>
+                </li>
+                <li>
+                    <a href="/"><span>Section</span></a>
+                </li>
+                <li>
+                    <a href="/"><span>Section</span></a>
+                </li>
+                <li>
+                    <a href="/"><span>Section</span></a>
+                </li>
+            </ul>
+            <ul class="footer_menu--list">
+                <li>
+                    <a href="/contacts"><h5>Contact Us</h5></a>
+                </li>
+                <li>
+                    <a href="/"><span>Section</span></a>
+                </li>
+            </ul>
+        </nav>
+        <div class="footer_policy">
+            <a href="/">© 2025 ZSMiO nr 5</a>
+            <a href="/terms">Terms</a>
+            <a href="/security">Security</a>
+            <a href="/privacy">Privacy</a>
+        </div>
     </div>
-  </div>
-  <SocialMediaBlock className="footer_socials" />
+    <SocialMediaBlock className="footer_socials" />
 </footer>
 
 <style>
-  footer {
-    display: grid;
-    grid-template-columns: 1fr 2fr auto;
-    gap: var(--std-space);
-    font-family: Arial, Helvetica, sans-serif;
-    margin-bottom: var(--std-space);
-  }
-
-  footer > iframe {
-    align-self: stretch;
-    justify-self: stretch;
-    border-radius: var(--std-radius);
-  }
-
-  .footer--content {
-    display: flex;
-    flex-direction: column;
-    gap: var(--std-space);
-  }
-
-  .footer_menu {
-    background-color: var(--gray-color);
-    padding: var(--std-padding);
-    border-radius: var(--std-radius);
-    display: flex;
-    gap: 32px;
-    justify-content: center;
-  }
-
-  .footer_menu--list {
-    display: flex;
-    flex-direction: column;
-    gap: var(--std-space);
-  }
-
-  .footer_menu--list h5 {
-    margin-bottom: 12px;
-  }
-
-  .footer_menu--list span {
-    color: var(--p-color);
-  }
-
-  :global(.footer_socials) {
-    flex-direction: column;
-  }
-
-  .footer_policy {
-    background-color: var(--gray-color);
-    padding: var(--std-padding);
-    border-radius: var(--std-radius);
-    display: flex;
-    justify-content: center;
-    gap: 32px;
-  }
-
-  .footer_policy > a {
-    color: var(--p-color);
-  }
-
-  .footer--content > nav,
-  .footer--content > div {
-    transition: 0.3s;
-  }
-
-  .footer--content > nav:hover,
-  .footer--content > div:hover {
-    background-color: var(--gray-lighter-color);
-  }
-
-  @media (max-width: 1040px) {
     footer {
-      grid-template-columns: 1fr;
-      grid-template-rows: 1fr 1fr auto;
+        display: grid;
+        grid-template-columns: 1fr 2fr auto;
+        gap: var(--std-space);
+        font-family: Arial, Helvetica, sans-serif;
+        margin-bottom: var(--std-space);
     }
-    :global(.footer_socials) {
-      flex-direction: row;
-    }
-  }
 
-  @media (max-width: 600px) {
-    .footer_menu--list > li:not(:first-child) {
-      display: none;
+    footer > iframe {
+        align-self: stretch;
+        justify-self: stretch;
+        border-radius: var(--std-radius);
     }
-    .footer_menu,
+
+    .footer--content {
+        display: flex;
+        flex-direction: column;
+        gap: var(--std-space);
+    }
+
+    .footer_menu {
+        background-color: var(--gray-color);
+        padding: var(--std-padding);
+        border-radius: var(--std-radius);
+        display: flex;
+        gap: 32px;
+        justify-content: center;
+    }
+
+    .footer_menu--list {
+        display: flex;
+        flex-direction: column;
+        gap: var(--std-space);
+    }
+
+    .footer_menu--list h5 {
+        margin-bottom: 12px;
+    }
+
+    .footer_menu--list span {
+        color: var(--p-color);
+    }
+
+    :global(.footer_socials) {
+        flex-direction: column;
+    }
+
     .footer_policy {
-      flex-direction: column;
-      gap: var(--std-space);
-      align-items: center;
-      padding: 16px;
+        background-color: var(--gray-color);
+        padding: var(--std-padding);
+        border-radius: var(--std-radius);
+        display: flex;
+        justify-content: center;
+        gap: 32px;
     }
-  }
+
+    .footer_policy > a {
+        color: var(--p-color);
+    }
+
+    .footer--content > nav,
+    .footer--content > div {
+        transition: 0.3s;
+    }
+
+    .footer--content > nav:hover,
+    .footer--content > div:hover {
+        background-color: var(--gray-lighter-color);
+    }
+
+    @media (max-width: 1040px) {
+        footer {
+            grid-template-columns: 1fr;
+            grid-template-rows: 1fr 1fr auto;
+        }
+        :global(.footer_socials) {
+            flex-direction: row;
+        }
+    }
+
+    @media (max-width: 600px) {
+        .footer_menu--list > li:not(:first-child) {
+            display: none;
+        }
+        .footer_menu,
+        .footer_policy {
+            flex-direction: column;
+            gap: var(--std-space);
+            align-items: center;
+            padding: 16px;
+        }
+    }
 </style>
--- src/lib/components/templates/Header.svelte
+++ src/lib/components/templates/Header.svelte
@@ -1,113 +1,113 @@
 <script>
-  import { onMount } from "svelte";
-  import ButtonLink from "$lib/components/ui/ButtonLink.svelte";
-  import ButtonLogin from "$lib/components/ui/ButtonLogin.svelte";
-  import favicon from "$lib/assets/favicon.svg";
+    import { onMount } from "svelte";
+    import ButtonLink from "$lib/components/ui/ButtonLink.svelte";
+    import ButtonLogin from "$lib/components/ui/ButtonLogin.svelte";
+    import favicon from "$lib/assets/favicon.svg";
 
-  var isOpen;
-  function toggleMenu() {
-    isOpen = !isOpen;
-  }
+    var isOpen;
+    function toggleMenu() {
+        isOpen = !isOpen;
+    }
 </script>
 
 <header>
-  <nav class="main_menu">
-    <a href="/" class="main_menu--logo">
-      <img src={favicon} alt="" />
-    </a>
-    <ul class="main_menu--list">
-      <li><ButtonLink href="/" text="Home" /></li>
-      <li><ButtonLink href="/clubs" text="Clubs" /></li>
-      <li><ButtonLink href="/projects" text="Projects" /></li>
-      <li><ButtonLink href="/calendar" text="Calendar" /></li>
-      <li><ButtonLink href="/contact" text="Contact Us" /></li>
-      <li><ButtonLogin href="/auth" text="Log in" /></li>
-    </ul>
-    <button aria-label="menu" class="burger_menu" on:click={toggleMenu}>
-      <hr />
-      <hr />
-      <hr />
-    </button>
-    <ul class="burger_menu--list" class:open={isOpen}>
-      <li><a href="/" on:click={toggleMenu}>Home</a></li>
-      <li><a href="/clubs" on:click={toggleMenu}>Clubs</a></li>
-      <li><a href="/" on:click={toggleMenu}>Projects</a></li>
-      <li><a href="/calendar" on:click={toggleMenu}>Calendar</a></li>
-      <li><a href="/contact" on:click={toggleMenu}>Contact Us</a></li>
-      <li><a href="/auth" on:click={toggleMenu}>Log in</a></li>
-    </ul>
-  </nav>
+    <nav class="main_menu">
+        <a href="/" class="main_menu--logo">
+            <img src={favicon} alt="" />
+        </a>
+        <ul class="main_menu--list">
+            <li><ButtonLink href="/" text="Home" /></li>
+            <li><ButtonLink href="/clubs" text="Clubs" /></li>
+            <li><ButtonLink href="/projects" text="Projects" /></li>
+            <li><ButtonLink href="/calendar" text="Calendar" /></li>
+            <li><ButtonLink href="/contact" text="Contact Us" /></li>
+            <li><ButtonLogin href="/auth" text="Log in" /></li>
+        </ul>
+        <button aria-label="menu" class="burger_menu" on:click={toggleMenu}>
+            <hr />
+            <hr />
+            <hr />
+        </button>
+        <ul class="burger_menu--list" class:open={isOpen}>
+            <li><a href="/" on:click={toggleMenu}>Home</a></li>
+            <li><a href="/clubs" on:click={toggleMenu}>Clubs</a></li>
+            <li><a href="/projects" on:click={toggleMenu}>Projects</a></li>
+            <li><a href="/calendar" on:click={toggleMenu}>Calendar</a></li>
+            <li><a href="/contact" on:click={toggleMenu}>Contact Us</a></li>
+            <li><a href="/auth" on:click={toggleMenu}>Log in</a></li>
+        </ul>
+    </nav>
 </header>
 
 <style>
-  header {
-    position: sticky;
-    top: 0;
-    z-index: 10;
-    background-color: var(--background-color);
-    /* background-color: #0008;
+    header {
+        position: sticky;
+        top: 0;
+        z-index: 10;
+        background-color: var(--background-color);
+        /* background-color: #0008;
     backdrop-filter: blur(32px); */
-  }
-
-  .main_menu {
-    position: relative;
-    z-index: 10;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    padding: var(--std-space) 0;
-  }
-
-  .main_menu--logo {
-    height: 36px;
-    img {
-      height: 100%;
     }
-  }
 
-  .main_menu--list {
-    display: flex;
-    gap: var(--std-space);
-  }
+    .main_menu {
+        position: relative;
+        z-index: 10;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        padding: var(--std-space) 0;
+    }
 
-  .burger_menu {
-    width: 32px;
-    background: none;
-    border: none;
-    padding: 0;
-    display: none;
-  }
+    .main_menu--logo {
+        height: 36px;
+        img {
+            height: 100%;
+        }
+    }
 
-  .burger_menu--list {
-    height: 100vh;
-    width: -webkit-fill-available;
-    background-color: var(--background-color);
-    /* display: none; */
-    position: absolute;
-    z-index: -1;
-    top: -100vh;
-    display: flex;
-    flex-direction: column;
-    gap: 24px;
-    justify-content: center;
-    align-items: center;
-    transition: 0.3s;
-  }
-
-  .burger_menu--list.open {
-    top: 0;
-  }
-
-  .burger_menu > hr {
-    border: 1px var(--text-color) solid;
-  }
-
-  @media (max-width: 600px) {
     .main_menu--list {
-      display: none;
+        display: flex;
+        gap: var(--std-space);
     }
+
     .burger_menu {
-      display: block;
+        width: 32px;
+        background: none;
+        border: none;
+        padding: 0;
+        display: none;
     }
-  }
+
+    .burger_menu--list {
+        height: 100vh;
+        width: -webkit-fill-available;
+        background-color: var(--background-color);
+        /* display: none; */
+        position: absolute;
+        z-index: -1;
+        top: -100vh;
+        display: flex;
+        flex-direction: column;
+        gap: 24px;
+        justify-content: center;
+        align-items: center;
+        transition: 0.3s;
+    }
+
+    .burger_menu--list.open {
+        top: 0;
+    }
+
+    .burger_menu > hr {
+        border: 1px var(--text-color) solid;
+    }
+
+    @media (max-width: 600px) {
+        .main_menu--list {
+            display: none;
+        }
+        .burger_menu {
+            display: block;
+        }
+    }
 </style>
